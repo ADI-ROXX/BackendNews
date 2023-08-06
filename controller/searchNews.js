a840f377f55a680bac7c30110db9313e48b9be12
ADI-ROXX <110272990+ADI-ROXX@users.noreply.github.com>
2023-08-07 00:32:44 +0530

    test5

--- controller/searchNews.js
+++ controller/searchNews.js
@@ -2,17 +2,21 @@
 
 export const searchNews=async(req,res)=>{
     const search=req.body.search;;
-    console.log("here");
+    console.log("First");
     try {    
         let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d114f7f8a8bd42fc9b85336ee25c7463&page=${1}&pageSize=20`;
         if(search!=''){
             url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d114f7f8a8bd42fc9b85336ee25c7463&q=${search}&sortBy=popularity&page=${1}&pageSize=20`;
 
         }
-        console.log("hereeddd");
+        console.log("Second");
         let data = await fetch(url);
+
+        console.log("Third");
         let parsed_data = await data.json();
+        console.log("Fourth");
         res.status(200).json({data:parsed_data});
+        console.log("Fifth");
 
 
     } catch(e){
