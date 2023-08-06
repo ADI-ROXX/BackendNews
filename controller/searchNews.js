1637420d996c1ced7fd4d691379ce708e26fb236
ADI-ROXX <110272990+ADI-ROXX@users.noreply.github.com>
2023-08-06 22:14:35 +0530

    first commit

---
+++ controller/searchNews.js
@@ -0,0 +1,29 @@
+
+
+export const searchNews=async(req,res)=>{
+    const search=req.body.search;;
+    console.log("here");
+    console.log(search);
+    try {    
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d114f7f8a8bd42fc9b85336ee25c7463&page=${1}&pageSize=20`;
+        if(search==''){
+            url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d114f7f8a8bd42fc9b85336ee25c7463&q=${search}&sortBy=popularity&page=${1}&pageSize=20`;
+
+        }
+        let data = await fetch(url);
+        let parsed_data = await data.json();
+        res.status(200).json({data:parsed_data});
+
+
+    } catch(e){
+        res.status(405).json({msg:"Some error occured"})
+    }
+
+    
+
+
+
+
+
+}
+
